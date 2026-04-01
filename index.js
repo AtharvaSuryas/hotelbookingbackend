932be65484df8910fe42606aec2ebc3135c655ce
AtharvaSuryas <suryas.atharva18@gmail.com>
2026-04-01 19:00:15 +0530

    updted cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: [
-        "http://localhost:5000/api/auth",
+        "https://hotelbookingbackend-1-ezvs.onrender.com",
         "https://hotelbookingfrontendpvt.vercel.app/"
     ],
     credentials:true
