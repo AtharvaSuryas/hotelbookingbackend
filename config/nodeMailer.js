84e75c4d2dddfaac619dfec7dcca7240c7cae2de
AtharvaSuryas <suryas.atharva18@gmail.com>
2026-04-06 18:35:26 +0530

    changed port from string to num in nodemailer

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer'
 export const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: '587',
+    port: 587,
     secure: false,
     auth:{
         user: process.env.SMTP_USER,
